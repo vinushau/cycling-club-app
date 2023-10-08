611c5d99ea190bfee75984c5128b32b4365a3ce2
Luke Schultz <lukesz2004@gmail.com>
2023-10-08 16:12:37 -0400

    Connected app to Firebase

--- app/src/main/java/com/uotttawa/lschu105/gcccyclingapp/MainActivity.java
+++ app/src/main/java/com/uotttawa/lschu105/gcccyclingapp/MainActivity.java
@@ -6,6 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -13,13 +19,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     public void onLoginButton(View view) {
+        /*Test
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Test");
+        */
+
+        //Navigates to login page
         Intent intent = new Intent(getApplicationContext(), login.class);
         startActivityForResult(intent, 0);
     }
+
     public void onRegisterButton(View view) {
+        // Navigates to the register page
         Intent intent = new Intent(getApplicationContext(), register.class);
         startActivityForResult(intent, 0);
     }
-
 }
