16a1ac0aefc60adc012aab37d881dde309eb6ecf
Luke Schultz <lukesz2004@gmail.com>
2023-10-09 17:43:36 -0400

    Account role added to welcome page

--- app/src/main/java/com/uotttawa/lschu105/gcccyclingapp/WelcomePage.java
+++ app/src/main/java/com/uotttawa/lschu105/gcccyclingapp/WelcomePage.java
@@ -1,31 +1,41 @@
 package com.uotttawa.lschu105.gcccyclingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class WelcomePage extends AppCompatActivity {
 
     FirebaseAuth auth;
     Button button;
     TextView textView;
+    TextView roles; // TextView for displaying roles
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
 
+        // Reference to the Firebase Realtime Database
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+
         auth = FirebaseAuth.getInstance();
         button = findViewById(R.id.logout);
         textView = findViewById(R.id.user_details);
+        roles = findViewById(R.id.roles);
+
         FirebaseUser user = auth.getCurrentUser();
+
         if (user == null){
             Intent intent = new Intent(getApplicationContext(), Login.class);
             startActivity(intent);
@@ -34,12 +44,27 @@ public class WelcomePage extends AppCompatActivity {
         else{
             String username = user.getDisplayName();
             String email = user.getEmail();
+            DatabaseReference accountTypeReference = database.child(username);
 
-            if (username != null && !username.isEmpty()) {
-                textView.setText(username);
-            } else if (email != null) {
-                textView.setText(email);
-            }
+            accountTypeReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    String accountTypeValue = dataSnapshot.getValue(String.class);
+                    System.out.println(username + ": " + accountTypeValue);
+
+                    if (username != null && !username.isEmpty()) {
+                        textView.setText(username);
+                        roles.setText(accountTypeValue);
+                    } else if (email != null) {
+                        textView.setText(email);
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    System.err.println("Error: " + databaseError.getMessage());
+                }
+            });
         }
 
         button.setOnClickListener(new View.OnClickListener() {
