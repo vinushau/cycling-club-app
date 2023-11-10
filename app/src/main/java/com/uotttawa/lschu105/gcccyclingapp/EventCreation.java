c6cceba9e156f14e290890bd5d108616e65ebc34
Luke Schultz <lukesz2004@gmail.com>
2023-11-09 19:02:54 -0500

    Event creation retrieves requirements from database

--- app/src/main/java/com/uotttawa/lschu105/gcccyclingapp/EventCreation.java
+++ app/src/main/java/com/uotttawa/lschu105/gcccyclingapp/EventCreation.java
@@ -1,47 +1,43 @@
 package com.uotttawa.lschu105.gcccyclingapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
-import android.util.Log;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Space;
 import android.widget.Spinner;
 import android.widget.TextView;
-
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.*;
 
 public class EventCreation extends AppCompatActivity {
 
     private EditText eventNameField;
-    private EditText ageField;
-    private EditText paceField;
     private Spinner levelSpinner;
-    private TextView eventTypes;
-    private TextView eventDescription;
+    private TextView eventTypes, eventDescription;
     private DatabaseReference databaseReference;
+    private LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_creation);
+
         Intent intent = getIntent();
         String buttonName = intent.getStringExtra("eventType");
+
         eventTypes = findViewById(R.id.EventType);
-        eventTypes.setText(buttonName);
         eventDescription = findViewById(R.id.EventDescription);
-        DatabaseReference eventTypeReference = FirebaseDatabase.getInstance().getReference().child("EventTypes").child(buttonName);
+
+        DatabaseReference eventTypeReference = FirebaseDatabase.getInstance().getReference("EventTypes").child(buttonName);
 
         eventTypeReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -51,6 +47,7 @@ public class EventCreation extends AppCompatActivity {
                     String description = dataSnapshot.child("description").getValue(String.class);
                     eventDescription.setText(description);
                     eventTypes.setText(groupName);
+                    createEditTextFields(dataSnapshot.child("Requirements"));
                 }
             }
 
@@ -60,24 +57,18 @@ public class EventCreation extends AppCompatActivity {
         });
 
         eventNameField = findViewById(R.id.eventNameField);
-        ageField = findViewById(R.id.ageField);
-        paceField = findViewById(R.id.paceField);
         levelSpinner = findViewById(R.id.levelSpinner);
+        linearLayout = findViewById(R.id.linearLayout);
 
-        databaseReference = FirebaseDatabase.getInstance().getReference().child("Events");
+        databaseReference = FirebaseDatabase.getInstance().getReference("Events");
         String[] levelOptions = {"Difficulty Level", "Beginner", "Intermediate", "Advanced"};
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, levelOptions) {
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, levelOptions) {
             @Override
-            public View getDropDownView(int position, View convertView, android.view.ViewGroup parent) {
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
                 View view = super.getDropDownView(position, convertView, parent);
-                if (position == 0) {
-                    ((android.widget.TextView) view).setTextColor(getResources().getColor(android.R.color.darker_gray));
-                    view.setEnabled(false);
-                } else {
-                    ((android.widget.TextView) view).setTextColor(getResources().getColor(android.R.color.black));
-                    view.setEnabled(true);
-                }
+                ((TextView) view).setTextColor(getResources().getColor(position == 0 ? android.R.color.darker_gray : android.R.color.black));
+                view.setEnabled(position != 0);
                 return view;
             }
         };
@@ -87,49 +78,81 @@ public class EventCreation extends AppCompatActivity {
         levelSpinner.setSelection(0);
 
         Button selectButton = findViewById(R.id.selectGroupRides);
-        selectButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                createEvent(buttonName);
-            }
-        });
+        selectButton.setOnClickListener(v -> createEvent(buttonName));
+    }
+
+    private void createEditTextFields(DataSnapshot requirementsSnapshot) {
+        int editTextWidthSP = 300;
+        int spaceHeightPixels = 25;
+
+        int editTextWidthPixels = (int) (editTextWidthSP * getResources().getDisplayMetrics().scaledDensity);
+
+        Space space = new Space(this);
+        space.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                spaceHeightPixels));
+        linearLayout.addView(space);
+
+        int index = 5;
+
+        for (DataSnapshot requirement : requirementsSnapshot.getChildren()) {
+            String requirementValue = requirement.getValue(String.class);
+            EditText editText = new EditText(this);
+            editText.setLayoutParams(new LinearLayout.LayoutParams(
+                    editTextWidthPixels,
+                    LinearLayout.LayoutParams.WRAP_CONTENT));
+            editText.setHint(requirementValue);
+            editText.setTextColor(getResources().getColor(R.color.black));
+            editText.setTextSize(20);
+            editText.setGravity(Gravity.CENTER);
+            linearLayout.addView(editText, index);
+            index++;
+        }
     }
 
     private void createEvent(String buttonName) {
         String eventName = eventNameField.getText().toString();
-        String age = ageField.getText().toString();
-        String pace = paceField.getText().toString();
         String difficultyLevel = levelSpinner.getSelectedItem().toString();
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         String username = user.getDisplayName();
 
-        DatabaseReference userEventsReference = FirebaseDatabase.getInstance().getReference().child("Users").child(username).child("Events");
+        DatabaseReference userEventsReference = FirebaseDatabase.getInstance().getReference("Users").child(username).child("Events");
 
         userEventsReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 long eventCount = dataSnapshot.getChildrenCount();
-
                 String nextIndex = String.valueOf(eventCount);
+
                 DatabaseReference eventReference = databaseReference.child(eventName);
                 eventReference.child("EventType").setValue(buttonName);
-                eventReference.child("Age").setValue(age);
-                eventReference.child("Pace").setValue(pace);
+
+                for (int i = 0; i < linearLayout.getChildCount(); i++) {
+                    if (linearLayout.getChildAt(i) instanceof EditText) {
+                        EditText editText = (EditText) linearLayout.getChildAt(i);
+                        String requirementKey = editText.getHint().toString().toLowerCase();
+                        String requirementValue = editText.getText().toString();
+                        eventReference.child(requirementKey).setValue(requirementValue);
+                    }
+                }
+
                 eventReference.child("Difficulty Level").setValue(difficultyLevel);
                 eventReference.child("CreatedBy").setValue(username);
 
-                DatabaseReference userEventsReference = FirebaseDatabase.getInstance().getReference().child("Users").child(username).child("Events").child(nextIndex);
+                DatabaseReference userEventsReference = FirebaseDatabase.getInstance().getReference("Users").child(username).child("Events").child(nextIndex);
                 userEventsReference.setValue(eventName);
 
                 eventNameField.setText("");
-                ageField.setText("");
-                paceField.setText("");
                 levelSpinner.setSelection(0);
+
+                Toast.makeText(EventCreation.this, "Event created successfully", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), WelcomePage.class);
+                startActivity(intent);
+                finish();
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-                Log.e("Database Error", "Error: " + databaseError.getMessage());
             }
         });
     }
